2082debe26b78b3194a2fb3e42b8df2cf44c6b4f
mariola <mariola@mariola-MS-7636>
2015-07-04 11:29:02 +0200

    remove dependience hipsnip-s3cmd

---
+++ cb_hnk/recipes/s3_deploy.rb
@@ -0,0 +1,9 @@
+require "s3_deploy/version"
+	
+	module S3Deploy
+	  def self.deploy(options={})
+	    to = options.delete(:to)
+	    from = options.delete(:from)
+	    `s3cmd sync --delete-removed  #{from}/ #{to}`
+	  end
+	end
