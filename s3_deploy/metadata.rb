08c5ac022e10163de76eaa1d81d1f597deb4581d
mariola <mariola@mariola-MS-7636>
2015-07-04 10:53:20 +0200

    add s3deploy

---
+++ s3_deploy/metadata.rb
@@ -0,0 +1,24 @@
+name             "s3_deploy"
+maintainer       "HipSnip Ltd."
+maintainer_email "adam@hipsnip.com"
+license          "Apache 2.0"
+description      "Provides a resource for downloading build packages and deploying them onto a server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.5.0"
+
+depends "hipsnip-s3cmd"
+
+attribute "s3_deploy/bucket",
+  :display_name => "S3 bucket",
+  :description => "The Amazon S3 bucket that contains all the builds",
+  :type => "string",
+  :required => "required",
+  :default => ""
+
+
+attribute "s3_deploy/deploy_root",
+  :display_name => "Deployment Root",
+  :description => "The folder in which you'll be creating this deployment",
+  :type => "string",
+  :required => "required",
+  :default => "/home/web"
