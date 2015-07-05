f4e40e1da604de841684af704c1c3b9a20fc7cc6
mariola <mariola@mariola-MS-7636>
2015-07-05 20:22:39 +0200

    add shippable.yml

--- cb_hnk/recipes/s3_deploy.rb
+++
@@ -1,9 +0,0 @@
-require "s3_deploy/version"
-	
-	module S3Deploy
-	  def self.deploy(options={})
-	    to = options.delete(:to)
-	    from = options.delete(:from)
-	    `s3cmd sync --delete-removed  #{from}/ #{to}`
-	  end
-	end
